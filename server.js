0416fd7b342ecc7d89264c6b653f49066d8eb7ff
gurusca <mytrosuper@proton.me>
2024-04-02 20:23:10 +0900

    1

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from "express"
 import path from "path"
 import * as url from 'url';
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
@@ -14,7 +18,7 @@ app.get("/", (req, res) => {
     res.sendFile(indexpath);
 })
 
-const PORT = 7860;
+const PORT = process.env.PORT;
 
 const handleListen = () => {
   console.log(`The server is running on the port ${PORT}`)
