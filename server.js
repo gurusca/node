f62752884c0fa37a7df7159bf386b2ae0c14bbd6
gurusca <mytrosuper@proton.me>
2024-04-02 20:57:57 +0900

    ws

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
 import express from "express"
-import path from "path"
-import * as url from 'url';
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
-
 const app = express();
-
-const indexpath = path.resolve(__dirname + '/public/index.html');
 
 app.use(express.static('public'));
 
 app.get("/", (req, res) => {
-    res.sendFile(indexpath);
+    res.send("WTF?");
 })
 
 const PORT = process.env.PORT;
